eace86f4d8a45471f4408c773393f5a88c9a3550
Bilal Şengül <60526366+bilalsengul@users.noreply.github.com>
2025-01-27 12:11:52 +0300

    Angular Localize Bug

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartService } from './services/cart.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgbModule]
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgbModule, HttpClientModule]
 })
 export class AppComponent implements OnInit {
   cartItemCount: number = 0;
